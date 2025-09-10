042ad361a47045bfeeaba70d0ea17f63ad88b40f
gukin-han <gukin.dev@gmail.com>
2025-09-11 07:57:40 +0900

    랭킹 상품 조회 주석 추가

--- apps/commerce-api/src/main/java/com/loopers/application/rank/RankFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/rank/RankFacade.java
@@ -5,6 +5,7 @@ import com.loopers.domain.brand.BrandService;
 import com.loopers.domain.product.Product;
 import com.loopers.domain.product.ProductService;
 import com.loopers.domain.rank.RankInMemoryRepository;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.stereotype.Service;
@@ -27,23 +28,26 @@ public class RankFacade {
     private final BrandService brandService;
 
     public RankResult.GetRankings getRankings(RankCommand.GetRankings command) {
+        // 1. paging
         String key = "ranking:all:" + command.date().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         long start = (long) (command.page() - 1) * command.size();
         long end = start + command.size() - 1;
 
+        // 2. top k 상품 리스트 조회
         Set<TypedTuple<String>> productIdsWithScores = rankInMemoryRepository.findTopRankings(key, start, end);
         if (productIdsWithScores == null || productIdsWithScores.isEmpty()) {
             return new RankResult.GetRankings(Collections.emptyList());
         }
-
         List<Long> sortedProductIds = productIdsWithScores.stream()
                 .map(TypedTuple::getValue)
+                .filter(Objects::nonNull)
                 .map(Long::parseLong)
                 .toList();
 
         Map<Long, Double> scoresMap = productIdsWithScores.stream()
                 .collect(Collectors.toMap(tuple -> Long.parseLong(tuple.getValue()), TypedTuple::getScore));
 
+        // 3. 상품, 브랜드 정보 조회
         List<Product> products = productService.findAllByIds(sortedProductIds);
         List<Long> brandIds = products.stream().map(Product::getBrandId).distinct().toList();
         List<Brand> brands = brandService.findAllByIds(brandIds);
